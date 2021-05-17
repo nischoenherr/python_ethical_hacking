e5b14710a04cd0f3eb090901eaca4fba3877e7a6
nischoenherr <schoenherr@ewerk.de>
2021-05-17 16:39:29 +0200

    add zip bruteforcer

---
+++ zip_file_pwd_bruteforcer/README.md
@@ -0,0 +1,12 @@
+# Zip File Brute Force
+
+## Introduction
+
+This Script was build to Brute Force Zip Files or Folders.
+
+## Installation 
+
+Use the Package Manager pip to install required libraries.
+
+```bash
+pip3 install tqdm
---
+++ zip_file_pwd_bruteforcer/index.py
@@ -0,0 +1,22 @@
+import zipfile
+from tqdm import tqdm
+
+wordlist = "john.txt"
+zip_file = "crack_me.zip"
+
+zip_file = zipfile.ZipFile(zip_file)
+
+n_words = len(list(open(wordlist, "rb")))
+
+print("Total passwords to test:", n_words)
+
+with open(wordlist, "rb") as wordlist:
+    for word in tqdm(wordlist, total=n_words, unit="word"):
+        try:
+            zip_file.extractall(pwd=word.strip())
+        except:
+            continue
+        else:
+            print("[+] Password found:", word.decode().strip())
+            exit(0)
+print("[!] Password not found, try other wordlist.")
